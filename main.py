a93123c92764605a295f0c5cd1fd63cbdc200093
Shashwat Saxena <shashwat.saxena612@gmail.com/usr/local/bin/python3>
2025-07-11 14:57:39 +0530

    Added payload generation logic for the AR parsed data

--- main.py
+++ main.py
@@ -4,19 +4,16 @@ import json
 from utils.config_loader import load_config
 from parser.AR_Parser import ARParser
 from parser.base_parser import BaseParser
-# from parser.employee_parser import EmployeeParser
-# from payload.payload_builder import build_payload
 
 config = load_config()
-print(config)
 
 PARSER_MAP = {
     "AR": ARParser,
 }
 
-def detect_parser(file_path):
+def detect_parser(file_path) -> BaseParser | ValueError:
     """Detect parser based on filename or config logic."""
-    if "AR" in file_path():
+    if "AR" in file_path:
         return PARSER_MAP["AR"]
     else:
         raise ValueError(f"No parser defined for: {file_path}")
@@ -30,19 +27,14 @@ def main():
     files = glob.glob(os.path.join(input_dir, "*.xlsx"))
     for file_path in files:
         try:
-            parser = ARParser(file_path)
-            parsed_data = parser.parse()
+            parser_class = detect_parser(file_path)
+            parser = parser_class(file_path)
+            payload = parser.parse()
             
-            # payload = build_payload(parsed_data)
+            output_file = os.path.join(output_dir, os.path.basename(file_path).replace(".xlsx", ".json"))
+            with open(output_file, "w") as f:
+                json.dump(payload, f, indent=2)
             
-            # Save to file
-            # output_file = os.path.join(output_dir, os.path.basename(file_path).replace(".xlsx", ".json"))
-            # with open(output_file, "w") as f:
-            #     json.dump(payload, f, indent=2)
-            
-            # logger.info(f"Saved JSON to {output_file}")
-
-            # # Optional: send to API
             # if config["api"]["enabled"]:
             #     response = post_payload(payload)
             #     logger.info(f"API response: {response.status_code} - {response.text}")
