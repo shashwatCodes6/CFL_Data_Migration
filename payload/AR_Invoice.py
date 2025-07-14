cf1dd6a66a6199b541cb3e5c87542cb8655d7459
shashwatCodes6 <shashwat.saxena612@gmail.com/usr/local/bin/python3>
2025-07-14 13:46:31 +0530

    test

--- payload/AR_Invoice.py
+++ payload/AR_Invoice.py
@@ -1,12 +1,16 @@
 class ARInvoice():
     def generate(self, parsed_data):
         res = []
-        
+
         for entry in parsed_data:
-            res.append({
-                "Legal Entity": entry["Legal Entity"],
-                "Invoice Number": entry["Invoice Number"],
-                "Line": entry["Line"]
-            })
+            try:
+                res.append({
+                    "Legal Entity": str(entry["Legal Entity"]),
+                    "Invoice Number": str(entry["Invoice Number"]),
+                    "Line": [str(i) for i in entry["Line"]],
+                    "Transaction Source": str(entry["Transaction Source"][0]),
+                })
+            except Exception as e:
+                print(f"Exception {e} on record {entry["Legal Entity"]}, {entry["Invoice Number"]}")
 
         return res
