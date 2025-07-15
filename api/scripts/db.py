b9b0c168242d446aa56836dfbfc5d33fd5f0ab50
shashwatCodes6 <shashwat.saxena612@gmail.com>
2025-07-15 19:00:32 +0530

    Added cookie and auth headers to api endpoint script

--- api/scripts/db.py
+++ api/scripts/db.py
@@ -6,20 +6,18 @@ from dotenv import load_dotenv
 # Load environment variables from .env
 load_dotenv()
 
-API_URL = os.getenv("API_URL")
-print(API_URL)
-
 def load_payload(file_path):
     with open(file_path, "r") as f:
         return json.load(f)
 
 def send_payload(payload):
-    headers = {"Content-Type": "application/json"}
+    headers = {"Content-Type": "application/json", "Authorization": os.getenv("AUTH_HEADER"), "Cookie": os.getenv("COOKIE")}
+    API_URL = os.getenv("API_URL")
     response = requests.post(API_URL, json=payload, headers=headers)
     return response
 
 def main():
-    json_path = "data/output/AR_input.json"
+    json_path = "data/output/Migration_template_AR_Credit Note.json"
 
     if not os.path.exists(json_path):
         print(f"File not found: {json_path}")
@@ -29,8 +27,10 @@ def main():
         payload = load_payload(json_path)
         for i in payload:
             response = send_payload(i)
-            print(f"Status: {response.status_code}")
-            print(f"Response: {response.text}")
+            if(response.status_code != 200):
+                print(f"Status: {response.status_code}")
+                print(f"Response: {response.text}")
+                break
     except Exception as e:
         print(f"Error occurred: {e}")
 
